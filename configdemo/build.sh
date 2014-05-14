5b5b60d969a85b4cb4ac8bdd80399131daaea924
Nathan Ross <nrossit2@gmail.com>
2014-05-14 08:17:42 -0400

    acknowledge bridge index.php's use as src. also small bugfix

--- configdemo/build.sh
+++ configdemo/build.sh
@@ -1,7 +1,8 @@
-
+#!/bin/bash
 cd `dirname ${BASH_SOURCE[0]}`
 python3 $PATH_TILTH --path-closure $PATH_CLOSURE --path-yui $PATH_YUICOMP -s "libs/*" "src/" -p"bridge/out"  "tilth_configdemo.csv"
 wait $!
+cp index.php bridge/
 python3 $PATH_TILTH -d --path-closure $PATH_CLOSURE --path-yui $PATH_YUICOMP -s "bridge/" -p"out/"  "tilth_configdemo.csv"
 wait $!
 cat out/bridge/index.php | sed -r "s/\.\.\/resources/resources/g" | sed -r "s/<\?php\s+\?>//g" > out.minified_inlined.html
